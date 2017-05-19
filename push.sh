8bdd5510d607fdc796600815528c816ddf9303df
mhoppal <michael.jam.hoppal@hpe.com>
2017-05-19 09:59:24 -0600

    Move key decryption to push as it is not always need

--- push.sh
+++ push.sh
@@ -33,10 +33,17 @@ git add *.tgz
 git add index.yaml
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 
-# Push Changes
+# Set up key to push
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
 chmod 600 ../deploy-key
 eval "$(ssh-agent -s)"
 ssh-add ../deploy-key
+
+# Push Changes
 git push "$SSH_REPO" gh-pages
 
 # Remove out directory
